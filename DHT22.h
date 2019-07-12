4915030a92da47bcfc4290a24eb4302d50dbc549
Nicolò Pavone <nicolo.pavone@tecnicosuperiorekennedy.it>
2019-07-12 14:14:18 +0200

    dht 22 update

---
+++ DHT22.h
@@ -0,0 +1,6 @@
+#include <p32xxxx.h>
+#include <plib.h> // Include the PIC32 Peripheral Library.
+
+void initDHT22();
+short readTemperature();
+short readHumidity();
--- SG90.h
+++
@@ -1,6 +0,0 @@
-#define SYSCLK 80000000L
-
-#define SERVO_PIN LATDbits.LATD8
-
-void initServo();
-void goTo(int target);
